572ddf3415470b4aa336b4e28fdda1c51fab2312
jGrish <martinezscjorge@gmail.com>
2019-11-22 13:59:28 +0100

    Componentes creados. Grid fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/sidenav/navbar/navbar.component';
+import { PlaylistComponent } from './components/sidenav/playlist/playlist.component';
+import { SearchComponent } from './components/header/search/search.component';
+import { UserComponent } from './components/header/user/user.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { PlayerComponent } from './components/player/player.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { AlbumListComponent } from './components/artist/album-list/album-list.component';
+import { SongListComponent } from './components/artist/song-list/song-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    PlaylistComponent,
+    SearchComponent,
+    UserComponent,
+    SidenavComponent,
+    PlayerComponent,
+    ArtistComponent,
+    AlbumListComponent,
+    SongListComponent
   ],
   imports: [
     BrowserModule,
