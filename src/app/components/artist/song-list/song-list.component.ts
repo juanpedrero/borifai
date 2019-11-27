162bc69538478bc8ee04d1e59d00970bded3aebb
juanpedrero <pedrero85@gmail.com>
2019-11-27 18:32:37 +0100

    cambios en songlist

--- src/app/components/artist/song-list/song-list.component.ts
+++ src/app/components/artist/song-list/song-list.component.ts
@@ -7,6 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SongListComponent implements OnInit {
 
+  songs = [
+    {
+      name_song: 'Gul lAbi',
+      time: `3:58`,
+      name_album: `Acid Arab`,
+    },
+    {
+      name_song: 'Macarena',
+      time: `2:58`,
+      name_album: `Acid Arab`,
+    },
+    {
+      name_song: 'Asereje',
+      time: `1:58`,
+      name_album: `Retamal`,
+    },
+    {
+      name_song: 'Bohemian Rapsody',
+      time: `7:58`,
+      name_album: `Forever Alone`,
+    },
+    {
+      name_song: 'Rosalia',
+      time: `3:20`,
+      name_album: `Mujer contra mujer`,
+    },
+    {
+      name_song: 'Gul lAbi',
+      time: `3:58`,
+      name_album: `Acid Arab`,
+    },
+    {
+      name_song: 'Gul lAbi',
+      time: `3:58`,
+      name_album: `Acid Arab`,
+    },
+    {
+      name_song: 'Gul lAbi',
+      time: `3:58`,
+      name_album: `Acid Arab`,
+    },
+    {
+      name_song: 'Gul lAbi',
+      time: `3:58`,
+      name_album: `Acid Arab`,
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
