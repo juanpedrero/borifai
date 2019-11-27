9c1339d685f5619b2d8419deb8844ec127ea341e
sebanob9 <52758114+sebanob9@users.noreply.github.com>
2019-11-27 17:33:22 +0100

    metodo para enseñar canciones creado no funciona

--- src/app/components/artist/artist.component.ts
+++ src/app/components/artist/artist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MusicService } from 'src/app/services/music.service';
 
 @Component({
   selector: 'app-artist',
@@ -6,16 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artist.component.scss']
 })
 export class ArtistComponent implements OnInit {
-  songsByAlbum = []
-  songs = []
-  constructor() { }
+  public songsByAlbum: any[];
+  public songs: any[];
+  /* songsByAlbum = [] */
+  /* songs = [] */
+  constructor(public musicService: MusicService) { 
+    this.songs = musicService.songs;
+  }
 
   ngOnInit() {
     // TODO LLamar al servicio de canciones y traer las canciones a la variable
-    // songs
+    
   }
   changeAlbumSelected(idAlbum){
-     this.songsByAlbum = this.songs.filter(s => s.idArtist === idAlbum )
+    debugger
+     this.songsByAlbum = this.songs.filter(s => s.albumId === idAlbum );
      // SongsByAlbum se actualiza cuando cambia el id album
   } 
 }
--- src/app/components/artist/song-list/song-list.component.ts
+++ src/app/components/artist/song-list/song-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MusicService } from 'src/app/services/music.service';
+
 
 @Component({
   selector: 'app-song-list',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SongListComponent implements OnInit {
 
-  constructor() { }
+  @Input() songs: any
+
+  constructor(public musicService: MusicService) { }
 
   ngOnInit() {
   }
